277dd82e52403eebbf11406e147dfdb7d2197d41
boniich <ezequieldbo25@gmail.com>
2023-04-14 15:56:24 -0300

    feat: add the function to select the type of clothe and his attributes

--- DesafioFinalQuark/Quotation.cpp
+++ DesafioFinalQuark/Quotation.cpp
@@ -1,4 +1,5 @@
 #include "Quotation.h";
+#include "Pants.h";
 #include <iostream>
 
 using namespace std;
@@ -13,19 +14,96 @@ Quotation::~Quotation()
 
 }
 
+int Quotation::getQuotationId()
+{
+	return 0;
+}
+void Quotation::setQuotationId()
+{
+
+}
+
+//date
+//
+
+
+int Quotation::getSellerId()
+{
+	return 0;
+}
+void Quotation::setSellerId(const int sellerId)
+{
+	_sellerId = sellerId;
+}
+
+// prenda
+
+
+void Quotation::selectingClothe(string option)
+{
+	if (option == "1")
+	{
+		
+	}
+	else if (option == "2")
+	{
+		_clothes = new Pants("","");
+	}
+}
+
+void Quotation::selectingQualityClothe(string option)
+{
+	if (option == "1")
+	{
+		// seteamos la calidad en standard
+		_clothes->setQuality("Standard");
+	}
+	else if (option == "2")
+	{
+		//seteamos la calidad en premium
+		_clothes->setQuality("Premium");
+	}
+}
+
+
+void Quotation::selectingUnitaryPrice(double unitaryPrice)
+{
+	_clothes->setUnitaryPrice(unitaryPrice);
+}
+
+void Quotation::selectingTypePants(string option)
+{
+
+	if (option == "1")
+	{
+		// definimos si el pantaon es chupin
+		static_cast<Pants*>(_clothes)->setTypePants("Chupin");
+	}
+	else if (option == "2")
+	{
+		// definimos si el pantalon es normal
+
+		static_cast<Pants*>(_clothes)->setTypePants("Normal");
+	}
+	
+}
+
+
+/// amount
+
+int Quotation::getAmount()
+{
+	return _amount;
+}
+void Quotation::setAmount(const int amount)
+{
+	_amount = amount;
+}
+
+
 void Quotation::makeQuotation()
 {
 	cout << "Realizando cotizacion" << endl;
 }
 
 
-/*
-void Quotation::showHistoryQuotationBySeller(const int seller_id) 
-{
-	id = 1;
-	date = "hoy";
-	this->seller_id = seller_id;
-	//prenda
-	amount = 1;
-	finalPrice = 10;
-}*/
--- DesafioFinalQuark/Quotation.h
+++ DesafioFinalQuark/Quotation.h
@@ -1,22 +1,48 @@
 #pragma once
 
+#include "Clothes.h";
 #include <string>
 
 using namespace std;
 
+class Clothes;
+
 class Quotation
 {
 private: 
-	int id;
-	string date;
-	int seller_id;
-	//prenda
-	int amount;
-	double finalPrice;
+	int _id;
+	string _date;
+	int _sellerId;
+	Clothes* _clothes = NULL;
+	int _amount = 0;
+	double _finalPrice;
 
 public:
 	Quotation();
 	~Quotation();
+
+	int getQuotationId();
+	void setQuotationId();
+
+	//date
+	//
+	int getSellerId();
+	void setSellerId(const int sellerId);
+
+	// clothes method
+
+	void selectingClothe(string option);
+	void selectingQualityClothe(string option);
+	void selectingUnitaryPrice(double unitaryPrice);
+
+	// clothes methods - pants
+
+	void selectingTypePants(string option);
+
+	int getAmount();
+	void setAmount(const int amount);
+
+
 	void makeQuotation();
 	void showHistoryQuotationBySeller(const int seller_id);
 
