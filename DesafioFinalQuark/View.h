ba1904ca857a5c728c1b51364fb705fa03d3ac85
boniich <ezequieldbo25@gmail.com>
2023-04-17 22:30:07 -0300

    refactor: change visibiliy of a lot of methods of view class that is not necesary that will be public

--- DesafioFinalQuark/View.h
+++ DesafioFinalQuark/View.h
@@ -11,16 +11,9 @@ class View : public IView
 {
 private:
 	Presenter* _presenter;
+
 	void showInitialMenu();
 	void optionSelected(const char* option, bool& exit);
-
-	
- public:
-	View();
-	~View() override;
-	void showText(const char* text) override;
-	void showText(const string& text) override;
-	void showHistoryQuotation(const list<Quotation*>& list) override;
 	void showQuotationMenu();
 	void showHeaderSubMenus();
 	void showChoosingClotheMenu();
@@ -30,4 +23,11 @@ private:
 	void comeBackToMainMenu();
 	void showQuotationResult();
 
+	
+ public:
+	View();
+	~View() override;
+	void showText(const char* text) override;
+	void showText(const string& text) override;
+	void showHistoryQuotation(const list<Quotation*>& list) override;
 };
